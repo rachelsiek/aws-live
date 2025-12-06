888065b729a6f533543f03221995697c418010fc
rachelsiek <siekjy-pm24@student.tarc.edu.my>
2025-12-06 12:39:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.cxdojjzcc46o.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee_db"
+custombucket = "jiaying-bucket"
+customregion = "us-east-1"
+
